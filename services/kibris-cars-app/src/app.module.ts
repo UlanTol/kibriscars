6b0ad46a5ae45517aaebc8693b41dbc1814cb357
UlanTol <104356514+UlanTol@users.noreply.github.com>
2023-07-25 17:08:48 +0600

    backend with db is done

--- services/kibris-cars-app/src/app.module.ts
+++ services/kibris-cars-app/src/app.module.ts
@@ -3,10 +3,22 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [
+    ConfigModule.forRoot(),
+    DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
--- services/kibris-cars-app/src/database/database.module.ts
+++ services/kibris-cars-app/src/database/database.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
-import { Connection } from 'typeorm';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_NAME'),
+        entities: ['dist/**/entities/*{.ts,.js}'],
+        synchronize: true,
+        migrations: ['dist/migrations/*{.ts,.js}'],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   exports: [TypeOrmModule],
 })
 export class DatabaseModule {
   constructor(connection: Connection) {
-    if (connection.isInitialized) {
-      console.log('DB connected');
-    }
+    if (connection.isConnected) console.log('DB Connected Successfully!');
   }
 }
